2be61544f22ae1cd0ab332a8bef6b39314c87ddf
Gabriele Perna <gabrigionny@gmail.com>
2025-10-06 11:42:06 +0200

    Started creating the expense class

---
+++ src/Classes.py
@@ -0,0 +1,44 @@
+import datetime
+from dataclasses import dataclass
+from enum import Enum
+
+@dataclass
+class ExpenseType(Enum):
+    BALANCER = 0,
+    FOODANDDRINKS = 1,
+    CLOTHES = 2,
+    VIDEOGAMES = 3,
+    POKEMONTCG = 4,
+    BARBER = 5,
+    GAS = 6,
+    BOOKS = 7,
+    TRAINTICKETS = 8,
+    BUSTICKETS = 9,
+    FINES = 10,
+    GIFTSANDFAVOURS = 11,
+    PACKAGESANDLETTERS = 12,
+    MOVIETHEATER = 13,
+    THEATER = 14,
+    EVENTS = 15,
+    ATTRACTIONS = 16,
+    LENDINGS = 17,
+    SCHOOLSUPPLIES = 18,
+    COINSJAR = 19,
+    RENTALS = 20,
+    PAPERS = 21,
+    LOST = 22,
+    STOLEN = 23,
+    OTHER = 24
+
+@dataclass
+class Expense:
+    amount: float
+    day: datetime.datetime
+    expenseType: ExpenseType
+
+    def __init__(self,amount,day,expenseTime):
+        self.amount = amount
+        self.day = day
+        self.expenseType = expenseTime
+
+
